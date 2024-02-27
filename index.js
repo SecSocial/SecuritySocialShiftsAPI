4e273cda8e0e8374fa5455fcbf3a0561375b9d87
Mohamed Abduljalil <meet.sp.com.sa@gmail.com>
2024-02-27 18:31:51 +0200

    added security.com.sa shifts

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // run `node index.js` in the terminal
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 var app = express();
-var fs = require('fs');
+var fs = require("fs");
 
 app.use(cors());
 
@@ -11,14 +11,14 @@ app.use(cors());
 //   optionsSuccessStatus: 200,
 // };
 
-app.get('/getShift', function (req, res) {
+app.get("/getShift", function (req, res) {
   // Get current time object with locale set to Riyadh
   function getCurrentTimeKSA() {
-    return new Date().toLocaleString('en-US', {
-      timeZone: 'Asia/Riyadh',
+    return new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Riyadh",
       hour12: false,
-      hour: '2-digit',
-      minute: '2-digit',
+      hour: "2-digit",
+      minute: "2-digit",
     });
   }
 
@@ -38,7 +38,7 @@ app.get('/getShift', function (req, res) {
   }
 
   // Read the shifts file
-  fs.readFile(__dirname + '/' + 'shifts.json', 'utf8', function (err, data) {
+  fs.readFile(__dirname + "/" + "shifts.json", "utf8", function (err, data) {
     // Store shifts data
     const shifts = JSON.parse(data);
     let shift = getCurrentShift(shifts, getCurrentTimeKSA());
@@ -46,8 +46,48 @@ app.get('/getShift', function (req, res) {
   });
 });
 
+// Shifts for security.com.sa
+app.get("/securitycomsaShifts", function (req, res) {
+  // Read the shifts file
+  fs.readFile(
+    __dirname + "/" + "shifts-securitycomsa.json",
+    "utf8",
+    function (err, data) {
+      // Store shifts data
+      const shifts = JSON.parse(data);
+      let shift = getCurrentShift(shifts, getCurrentTimeKSA());
+      res.end(JSON.stringify(shift));
+    }
+  );
+});
+
+// Get current time object with locale set to Riyadh
+function getCurrentTimeKSA() {
+  return new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Riyadh",
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+
+// Get current shift helper function
+function getCurrentShift(shifts, current_time) {
+  for (let i = 0; i < shifts.length; i++) {
+    if (current_time < shifts[i].starts) {
+      if (i === 0) {
+        return shifts.slice(-1)[0];
+      } else {
+        return shifts[i - 1];
+      }
+    } else if (i === shifts.length - 1) {
+      return shifts[i];
+    }
+  }
+}
+
 var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
 });
